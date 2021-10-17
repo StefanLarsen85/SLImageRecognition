12093fdcc0c83220694d07cb1678f7247a1f502b
Stefan Larsen <stefan@twol.dk>
2021-10-17 20:41:29 +0200

    Image analyze partially implemented

---
+++ Client/ConfigurationHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Client
+{
+    public static class ConfigurationHelper
+    {
+        public static string GetConfig(string key) => System.Environment.GetEnvironmentVariable(key);
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,13 +1,19 @@
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BlazorApp.Client
 {
     public class Program
     {
+        static string subscriptionKey = ConfigurationHelper.GetConfig("AzureCognitiveKey");
+        static string endpoint = ConfigurationHelper.GetConfig("AzureCognitiveEndpoint");
+
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -17,6 +23,44 @@ namespace BlazorApp.Client
             builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
 
             await builder.Build().RunAsync();
+        }
+
+        // Create a client
+        ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
+
+        // Analyze an image to get features and other properties.
+        AnalyzeImageUrl(client, ANALYZE_URL_IMAGE).Wait();
+
+
+        public static ComputerVisionClient Authenticate(string endpoint, string key)
+        {
+            ComputerVisionClient client =
+              new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+              { Endpoint = endpoint };
+            return client;
+        }
+
+        // URL image used for analyzing an image (image of puppy)
+        private const string ANALYZE_URL_IMAGE = "https://moderatorsampleimages.blob.core.windows.net/samples/sample16.png";
+
+        public static async Task AnalyzeImageUrl(ComputerVisionClient client, string imageUrl)
+        {
+            List<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>()
+            {
+                VisualFeatureTypes.Categories,
+                VisualFeatureTypes.Description,
+                VisualFeatureTypes.Faces,
+                VisualFeatureTypes.ImageType,
+                VisualFeatureTypes.Tags,
+                VisualFeatureTypes.Adult,
+                VisualFeatureTypes.Color,
+                VisualFeatureTypes.Brands,
+                VisualFeatureTypes.Objects
+            };
+
+            // Analyze the URL image 
+            ImageAnalysis results = await client.AnalyzeImageAsync(imageUrl, visualFeatures: features);
+
         }
     }
 }
