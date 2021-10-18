103695360ebbf278c948392664a760a5c17ef6cb
Stefan Larsen <stefan@twol.dk>
2021-10-18 21:36:30 +0200

    New test with ComputerVision

--- Api/AnalyzeImage.cs
+++ Api/AnalyzeImage.cs
@@ -7,6 +7,10 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 
 namespace BlazorApp.Api
 {
@@ -16,10 +20,54 @@ namespace BlazorApp.Api
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, ILogger log, ExecutionContext context)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            string subscriptionKey = config["ComputerVisionKey"];
+            string endpoint = "https://slimagerecognition.cognitiveservices.azure.com/";
+
+            //Create a client
+            ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
+
+            // URL image used for analyzing an image (image of puppy)
+            const string ANALYZE_URL_IMAGE = "https://moderatorsampleimages.blob.core.windows.net/samples/sample16.png";
+
+            // Analyze an image to get features and other properties.
+            AnalyzeImageUrl(client, ANALYZE_URL_IMAGE).Wait();
 
             var someObject = new { text = "This is the returnet message" };
 
             return new OkObjectResult(someObject);
+        }
+
+        public static ComputerVisionClient Authenticate(string endpoint, string key)
+        {
+            ComputerVisionClient client =
+              new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+              { Endpoint = endpoint };
+            return client;
+        }
+
+        public static async Task AnalyzeImageUrl(ComputerVisionClient client, string imageUrl)
+        {
+            List<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>()
+            {
+                VisualFeatureTypes.Categories,
+                VisualFeatureTypes.Description,
+                VisualFeatureTypes.Faces,
+                VisualFeatureTypes.ImageType,
+                VisualFeatureTypes.Tags,
+                VisualFeatureTypes.Adult,
+                VisualFeatureTypes.Color,
+                VisualFeatureTypes.Brands,
+                VisualFeatureTypes.Objects
+            };
+
+            // Analyze the URL image 
+            ImageAnalysis results = await client.AnalyzeImageAsync(imageUrl, visualFeatures: features);
         }
     }
 }
--- Client/Program.cs
+++ Client/Program.cs
@@ -11,9 +11,6 @@ namespace BlazorApp.Client
 {
     public class Program
     {
-        //static string subscriptionKey = ConfigurationHelper.GetConfig("AzureCognitiveKey");
-        //static string endpoint = ConfigurationHelper.GetConfig("AzureCognitiveEndpoint");
-
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -23,44 +20,6 @@ namespace BlazorApp.Client
             builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
 
             await builder.Build().RunAsync();
-        }
-
-        // Create a client
-        //ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
-
-        // Analyze an image to get features and other properties.
-        //AnalyzeImageUrl(client, ANALYZE_URL_IMAGE).Wait();
-
-
-        public static ComputerVisionClient Authenticate(string endpoint, string key)
-        {
-            ComputerVisionClient client =
-              new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
-              { Endpoint = endpoint };
-            return client;
-        }
-
-        // URL image used for analyzing an image (image of puppy)
-        private const string ANALYZE_URL_IMAGE = "https://moderatorsampleimages.blob.core.windows.net/samples/sample16.png";
-
-        public static async Task AnalyzeImageUrl(ComputerVisionClient client, string imageUrl)
-        {
-            List<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>()
-            {
-                VisualFeatureTypes.Categories,
-                VisualFeatureTypes.Description,
-                VisualFeatureTypes.Faces,
-                VisualFeatureTypes.ImageType,
-                VisualFeatureTypes.Tags,
-                VisualFeatureTypes.Adult,
-                VisualFeatureTypes.Color,
-                VisualFeatureTypes.Brands,
-                VisualFeatureTypes.Objects
-            };
-
-            // Analyze the URL image 
-            ImageAnalysis results = await client.AnalyzeImageAsync(imageUrl, visualFeatures: features);
-
         }
     }
 }
